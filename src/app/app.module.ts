5df1dcaced5d6b882654a19f7104806e1d7c111a
DESKTOP-ATUON1J\Magne <magne@alvheim.com>
2018-10-06 10:54:08 +0200

    Kap3.1 - Events list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventListComponent } from './events/event-list/event-list.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventListComponent
   ],
   bootstrap: [EventsAppComponent]
 })
