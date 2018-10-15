39939f05c009a237f935ba9209c11abe374a4716
DESKTOP-ATUON1J\Magne <magne@alvheim.com>
2018-10-15 22:34:28 +0200

    12 EventChiled settings for å lukke modal

--- src/app/common/simple-modal/simple-modal.component.ts
+++ src/app/common/simple-modal/simple-modal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from '../../services/jQuery.service';
 
 @Component({
   selector: 'simple-modal',
@@ -9,10 +10,15 @@ export class SimpleModalComponent implements OnInit {
 
   @Input() title: string;
   @Input() elementId: string;
+  @ViewChild('modalContainer') containerEl: ElementRef;
 
-  constructor() { }
+  constructor(@Inject(JQ_TOKEN) private $: any) { }
 
   ngOnInit() {
+  }
+
+  closeModal() {
+    this.$(this.containerEl.nativeElement).modal('hide');
   }
 
 }
